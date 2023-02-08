7188579bf6253e3086909bb66010d6afd5114e44
Lani Jung <lani009@naver.com>
2023-02-08 15:15:51 +0900

    add: gameserver models prototype

---
+++ BE/server/game_server/models.py
@@ -0,0 +1,15 @@
+class Player():
+    __id: str
+    __name: str
+
+    def __init__(self) -> None:
+        pass
+
+class HostPlayer(Player):
+    pass
+
+class RoomAttribute():
+    pass
+
+class Room():
+    pass
